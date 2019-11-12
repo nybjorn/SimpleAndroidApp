5ca6c6df08c734ed93efb3a4ee6d5de556ec8f76
Björn Nyberg <bjorn.nyberg@gmail.com>
2019-11-12 16:37:10 +0100

    Init. SAA with navigation controller

---
+++ app/src/main/java/com/example/simpleandroidapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.simpleandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        NavigationUI.setupActionBarWithNavController(this, findNavController(R.id.main_nav_fragment))
+        setupBottomNavMenu(findNavController(R.id.main_nav_fragment))
+    }
+
+    private fun setupBottomNavMenu(findNavController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav.setupWithNavController(findNavController)
+    }
+
+}
