a8bfcc90657af099b30dc34e332cda5199d1ee63
Björn Nyberg <bjorn.nyberg@gmail.com>
2019-11-22 10:03:10 +0100

    Upgrade libraries

--- app/src/main/java/com/example/simpleandroidapp/ui/third/ThirdFragment.kt
+++ app/src/main/java/com/example/simpleandroidapp/ui/third/ThirdFragment.kt
@@ -6,21 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
-import com.example.simpleandroidapp.MainActivity
 import com.example.simpleandroidapp.R
 import com.example.simpleandroidapp.repository.pojo.Beer
 import kotlinx.android.synthetic.main.fragment_third.*
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [ThirdFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [ThirdFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ThirdFragment : Fragment() {
 
     lateinit var beer: Beer
@@ -49,12 +39,10 @@ class ThirdFragment : Fragment() {
             one_beer_title.text = it.joinToString(separator = "\n") { food -> "* $food" }
         }
 
-        if(beer.imageUrl.isNullOrEmpty()) {
+        if (beer.imageUrl.isNullOrEmpty()) {
             beer_image.visibility = View.GONE
         } else {
             Glide.with(view).load(beer.imageUrl).into(beer_image)
         }
     }
 }
-
-
