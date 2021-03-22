0c3706a22a6c143e7ee1f20282feb43406961cdc
Björn Nyberg <bjorn.nyberg@gmail.com>
2021-03-22 17:04:51 +0100

    Latest versions, builds and runs but warnings.

--- app/src/main/java/com/example/simpleandroidapp/di/BoxModule.kt
+++ app/src/main/java/com/example/simpleandroidapp/di/BoxModule.kt
@@ -5,13 +5,13 @@ import com.example.simpleandroidapp.repository.dao.ObjectBox
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import io.objectbox.BoxStore
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object BoxModule {
     @Provides
     @Singleton
--- app/src/main/java/com/example/simpleandroidapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/simpleandroidapp/di/NetworkModule.kt
@@ -9,14 +9,14 @@ import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
     @Provides
     fun provideOkHttpClient(@ApplicationContext context: Context): OkHttpClient {
